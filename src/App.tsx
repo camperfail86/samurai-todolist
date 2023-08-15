a29c0c6c8fc0224c07690db37230738c4c57e363
camperfail86 <mega.kochkin@inbox.ru>
2023-08-16 02:27:49 +0500

    Добавляет первые файлы

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Todolist from "./components/Todolist";
+
+function App() {
+  return (
+    <div className="App">
+      <Todolist title='title'/>
+    </div>
+  );
+}
+
+// function hello() {
+//     return(
+//       <div>Hello</div>
+//     )
+// }
+
+export default App;
---
+++ src/components/Todolist.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+type PropsType = {
+  title?: string
+}
+
+function Todolist(props: PropsType) {
+  return (
+    <div>
+      <h3>What to {props.title}</h3>
+      <div>
+        <input/>
+        <button>click</button>
+      </div>
+      <ul>
+        <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+        <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+        <li><input type="checkbox" checked={false}/> <span>React</span></li>
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  )
+}
+
+export default Todolist;
